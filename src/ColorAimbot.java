0cfcaf9b0ee2b74be19daf8deb7a6191c99a966c
mwdewey <mwdewey@optonline.net>
2015-12-21 00:43:52 -0500

    Added algo for static viewpoint movement detection

--- src/ColorAimbot.java
+++ src/ColorAimbot.java
@@ -17,6 +17,8 @@ public class ColorAimbot {
         Rectangle captureSize = new Rectangle(x, y, width, height);
         ImageIcon icon = new ImageIcon();
         BufferedImage screenShotImage;
+        BufferedImage keyImage;
+        BufferedImage prevImage;
         JFrame captureFrame = new JFrame();
         JFrame previewFrame = new JFrame();
         List<BufferedImage> imageBuffer = new ArrayList<>();
@@ -29,29 +31,44 @@ public class ColorAimbot {
         captureFrame.getRootPane().setBorder(BorderFactory.createMatteBorder(4, 4, 4, 4, Color.RED));
         captureFrame.setVisible(true);
 
-        previewFrame.setLocation(x+600, y);
+        previewFrame.setLocation(x + 600, y);
         previewFrame.setSize(width, height);
         previewFrame.add(new JLabel(icon), BorderLayout.CENTER);
         previewFrame.setVisible(true);
 
+        int superCount = -1;
+        keyImage = null;
+        prevImage = null;
+        BufferedImage newimg;
+        boolean imgDiff = false;
         while (true) {
-            imageBuffer.add(robot.createScreenCapture(captureSize));
-            if(imageBuffer.size() > bufferLimit) imageBuffer.remove(0);
+            screenShotImage = robot.createScreenCapture(captureSize);
+            //imageBuffer.add(screenShotImage);
+            //if(imageBuffer.size() > bufferLimit) imageBuffer.remove(0);
 
-            for(BufferedImage img : imageBuffer){
-
-            }
-
+            newimg = new BufferedImage(width,height,BufferedImage.TYPE_3BYTE_BGR);
+            imgDiff = false;
             for(int w = 0; w < width; w++){
-                for(int h = 0; h < height; h++){
-                    //if(image.getRGB(w,h) == Color.white.getRGB()) image.setRGB(w,h,Color.black.getRGB());
-                    //else image.setRGB(w,h,Color.white.getRGB());
+                for(int h = 0; h < height && keyImage != null; h++){
+                    if(screenShotImage.getRGB(w,h) != keyImage.getRGB(w, h)) {
+                        newimg.setRGB(w,h,screenShotImage.getRGB(w,h));
+                    }
+
+                    if(!imgDiff && screenShotImage.getRGB(w,h) != prevImage.getRGB(w, h)) imgDiff = true;
                 }
             }
 
-            //icon.setImage(image);
+            if(!imgDiff) superCount++;
+
+            if(superCount > 30 || superCount == -1){
+                superCount = 0;
+                keyImage = screenShotImage;
+            }
+
+            icon.setImage(newimg);
             previewFrame.repaint();
             previewFrame.validate();
+            prevImage = screenShotImage;
         }
 
 
